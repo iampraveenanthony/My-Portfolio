1f00721643804d390a2c3dcb34b3d76bd71263c2
iampraveenanthony <yourspraveenleo11@gmail.com>
2024-11-13 19:09:33 +0530

    second commit

--- src/Pages/MyPortfolio.jsx
+++ src/Pages/MyPortfolio.jsx
@@ -1,5 +1,11 @@
-import data from "../data/index.json";
 import giticon from "../img/github.png";
+import calImage from "../img/Basic-Calculator.png";
+import AmaImage from "../img/Amazon.jpg";
+import cicImage from "../img/Cisco.jpg";
+import busImage from "../img/Bus-Booking.png";
+import bankImage from "../img/banksystem.png";
+import cdbImage from "../img/cdb.png";
+
 
 export default function MyPortfolio() {
   return (
@@ -22,48 +28,156 @@ export default function MyPortfolio() {
         </div>
       </div>
       <div className="portfolio--section--container">
-        {data?.portfolio?.map((item, index) => (
-          <div
-            key={index}
-            className="portfolio--section--card"
-            onClick={() => (window.location.href = item.view)}
-          >
-            <div className="portfolio--section--img">
-              <img src={item.src} alt="Placeholder" />
-            </div>
-            <div className="portfolio--section--card--content">
-              <div>
-                <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
-              </div>
-              <p className="text-sm portfolio--link">
-                <a href={item.link}>View in GitHub</a>
+        <div
+          className="portfolio--section--card"
+          onClick={() =>
+            (window.location.href =
+              "https://iampraveenanthony.github.io/Basic-Calculator/")
+          }
+        >
+          <div className="portfolio--section--img">
+            <img src={calImage} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">Basic Calculator</h3>
+              <p className="text-md">
+                This repository contains a simple calculator web application
+                built using HTML, CSS, and JavaScript. The application provides
+                a user-friendly interface for performing basic arithmetic
+                operations, including addition, subtraction, multiplication, and
+                division.
               </p>
             </div>
+            <p className="text-sm portfolio--link">
+              <a href="https://github.com/iampraveenanthony/Basic-Calculator">
+                View in GitHub
+              </a>
+            </p>
           </div>
-        ))}
+        </div>
+        <div
+          className="portfolio--section--card"
+          onClick={() =>
+            (window.location.href =
+              "https://iampraveenanthony.github.io/Amazon-Clone/")
+          }
+        >
+          <div className="portfolio--section--img">
+            <img src={AmaImage} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">Amazon Clone Website</h3>
+              <p className="text-md">
+              The website is a clone of Amazon, designed to showcase a shopping platform with a variety of products. It features product listings, a shopping cart, and a user-friendly interface. Users can browse electronics and other categories, add items to their cart, and proceed to checkout. The design aims to replicate Amazon's core functionality and layout, providing a familiar shopping experience for users.
+              </p>
+            </div>
+            <p className="text-sm portfolio--link">
+              <a href="https://github.com/iampraveenanthony/Amazon-Clone">
+                View in GitHub
+              </a>
+            </p>
+          </div>
+        </div>
+        <div
+          className="portfolio--section--card"
+          onClick={() =>
+            (window.location.href =
+              "https://iampraveenanthony.github.io/Cisco-Clone/")
+          }
+        >
+          <div className="portfolio--section--img">
+            <img src={cicImage} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">Cisco Clone Website</h3>
+              <p className="text-md">
+              The website is a clone of Cisco's platform, showcasing their network, software, and cybersecurity solutions. It highlights products like AI-powered Cisco XDR and Full-Stack Observability for IT automation. There are also sections for industry-specific solutions, customer stories, and insights into upcoming technologies such as AI and IoT. The site promotes Cisco’s involvement in digital transformation, security, and real-time IT support.
+              </p>
+            </div>
+            <p className="text-sm portfolio--link">
+              <a href="https://github.com/iampraveenanthony/Cisco-Clone">
+                View in GitHub
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
       <div className="portfolio--section--container">
-        {data?.portfolio2?.map((item, index) => (
-          <div
-            key={index}
-            className="portfolio--section--card"
-            onClick={() => (window.location.href = item.view)}
-          >
-            <div className="portfolio--section--img">
-              <img src={item.src} alt="Placeholder" />
-            </div>
-            <div className="portfolio--section--card--content">
-              <div>
-                <h3 className="portfolio--section--title">{item.title}</h3>
-                <p className="text-md">{item.description}</p>
-              </div>
-              <p className="text-sm portfolio--link">
-                <a href={item.link}>View in GitHub</a>
+        <div
+          className="portfolio--section--card"
+          onClick={() =>
+            (window.location.href =
+              "https://github.com/iampraveenanthony/Bus_Booking_System")
+          }
+        >
+          <div className="portfolio--section--img">
+            <img src={busImage} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">Bus Booking System</h3>
+              <p className="text-md">
+              The program is a bus booking system that allows users to register, log in, view available buses, and book tickets. Users provide their personal details for registration and login. The system displays available bus routes and details, allowing users to select a bus and proceed with booking by confirming the date of the journey and ticket details.
               </p>
             </div>
+            <p className="text-sm portfolio--link">
+              <a href="https://github.com/iampraveenanthony/Bus_Booking_System">
+                View in GitHub
+              </a>
+            </p>
           </div>
-        ))}
+        </div>
+        <div
+          className="portfolio--section--card"
+          onClick={() =>
+            (window.location.href =
+              "https://github.com/iampraveenanthony/Banking_System")
+          }
+        >
+          <div className="portfolio--section--img">
+            <img src={bankImage} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">Banking System</h3>
+              <p className="text-md">
+              This Python program simulates a simple banking system that allows users to perform basic transactions. Users can show their current balance, deposit money, withdraw money, or exit the program. The program includes input validation for negative amounts and insufficient balance during withdrawals.
+              </p>
+            </div>
+            <p className="text-sm portfolio--link">
+              <a href="https://github.com/iampraveenanthony/Banking_System">
+                View in GitHub
+              </a>
+            </p>
+          </div>
+        </div>
+        <div
+          className="portfolio--section--card"
+          onClick={() =>
+            (window.location.href =
+              "https://github.com/iampraveenanthony/College_DataBase")
+          }
+        >
+          <div className="portfolio--section--img">
+            <img src={cdbImage} alt="Placeholder" />
+          </div>
+          <div className="portfolio--section--card--content">
+            <div>
+              <h3 className="portfolio--section--title">College Database</h3>
+              <p className="text-md">
+              The college database consists of tables for student details, college bus details, and student mark statements, all managed using MySQL. It utilizes SQL features such as grouping and joins to efficiently retrieve and aggregate data across these related tables. This structure supports comprehensive queries for generating reports.
+              </p>
+            </div>
+            <p className="text-sm portfolio--link">
+              <a href="https://github.com/iampraveenanthony/College_DataBase">
+                View in GitHub
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/Pages/MySkills.jsx
+++ src/Pages/MySkills.jsx
@@ -1,4 +1,7 @@
-import data from "../data/index.json";
+import frontPng from "../img/frontend.png";
+import backPng from "../img/backend.png";
+import framePng from "../img/framework.png";
+import designPng from "../img/design.png";
 
 export default function MySkills() {
   return (
@@ -8,26 +11,51 @@ export default function MySkills() {
         <h2 className="skills--section--heading">My Expertise</h2>
       </div>
       <div className="skills--section--container">
-        {data?.skills?.map((item, index) => (
-          <div key={index} className="skills--section--card">
-            <div className="skills--section--img">
-              <img src={item.src} alt="Product Chain" />
-            </div>
-            <div className="skills--section--card--content">
-              <h3 className="skills--section--title">{item.title}</h3>
-              <p className="skills--section--description">{item.description}</p>
-              <p className="skills--section--description">
-                {item.description2}
-              </p>
-              <p className="skills--section--description">
-                {item.description3}
-              </p>
-              <p className="skills--section--description">
-                {item.description4}
-              </p>
-            </div>
+        <div className="skills--section--card">
+          <div className="skills--section--img">
+            <img src={frontPng} alt="Product Chain" />
           </div>
-        ))}
+          <div className="skills--section--card--content">
+            <h3 className="skills--section--title">Front-End</h3>
+            <p className="skills--section--description">✓ HTML</p>
+            <p className="skills--section--description">✓ CSS</p>
+            <p className="skills--section--description">✓ JavaScript</p>
+            <p className="skills--section--description">✓ Node JS</p>
+          </div>
+        </div>
+        <div className="skills--section--card">
+          <div className="skills--section--img">
+            <img src={backPng} alt="Product Chain" />
+          </div>
+          <div className="skills--section--card--content">
+            <h3 className="skills--section--title">Back-End</h3>
+            <p className="skills--section--description">✓ Python</p>
+            <p className="skills--section--description">✓ MySQL</p>
+          </div>
+        </div>
+        <div className="skills--section--card">
+          <div className="skills--section--img">
+            <img src={framePng} alt="Product Chain" />
+          </div>
+          <div className="skills--section--card--content">
+            <h3 className="skills--section--title">Framework</h3>
+            <p className="skills--section--description">✓ React Js</p>
+            <p className="skills--section--description">✓ Bootstrap</p>
+            <p className="skills--section--description">✓ Django</p>
+          </div>
+        </div>
+        <div className="skills--section--card">
+          <div className="skills--section--img">
+            <img src={designPng} alt="Product Chain" />
+          </div>
+          <div className="skills--section--card--content">
+            <h3 className="skills--section--title">Design</h3>
+            <p className="skills--section--description">✓ FIGMA</p>
+            <p className="skills--section--description">✓ CANVA</p>
+            <p className="skills--section--description">✓ Illustrator</p>
+            <p className="skills--section--description">✓ Photoshop"</p>
+          </div>
+        </div>
       </div>
     </section>
   );
